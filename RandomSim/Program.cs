f2e661cfcc8dc83b411bdab59a6a821fbdb080c9
naya <naya.ales@gmail.com>
2023-10-26 15:11:29 +0300

    Next predict

--- RandomSim/Program.cs
+++ RandomSim/Program.cs
@@ -9,10 +9,14 @@ if (!String.IsNullOrEmpty(currentDirectory))
     var eventsPath = currentDirectory + @"\events.csv";
     var adjectivesPath = currentDirectory + @"\adjectives.csv";
 
-    if (File.Exists(eventsPath) && File.Exists(adjectivesPath))
-        Prediction.Get(currentDirectory, eventsPath, adjectivesPath, name);
-    else
-        Console.WriteLine($"Error: Программный файл не найден: {eventsPath}.");
+    for (int i = 0; i < 100; i++)
+    {
+        i++;
+        if (File.Exists(eventsPath) && File.Exists(adjectivesPath))
+            Prediction.Get(currentDirectory, eventsPath, adjectivesPath, name);
+        else
+            Console.WriteLine($"Error: Программный файл не найден: {eventsPath}.");
+    }
 }
 else
 {
